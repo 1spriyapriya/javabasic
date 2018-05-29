b09baa370b5d301c68189fb7c8d2c3373be637fc
1spriyapriya <priyadharshinisekar001@gmail.com>
2018-05-29 18:12:11 +0530

    basic1

---
+++ javabasics/src/com/basic/ControlProgramExample.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class ControlProgramExample {
+	public static void main(String[]args){
+		int age=65;
+		if(age>=65)
+			System.out.println("age is greater or equal to 65");
+		else
+			System.out.println("age is less than 65");
+		
+			
+	}
+	
+	
+
+}
---
+++ javabasics/src/com/basic/DataTypeExample.java
@@ -0,0 +1,11 @@
+package com.basic;
+
+public class DataTypeExample {
+	public static void main(String []args) {
+		String firstName= "Priya";
+		String lastName= "sri";
+		System.out.println(firstName+" "+ lastName);
+		
+	}
+
+}
