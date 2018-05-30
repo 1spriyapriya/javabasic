fd4caaf7ff65b4fbf04e790ffc0e0179115dcd70
1spriyapriya <priyadharshinisekar001@gmail.com>
2018-05-30 14:44:38 +0530

    basic

---
+++ javabasics/src/com/basic/ForLoopExample.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class ForLoopExample {
+	public static void main(String []args)
+	{
+		int i;
+		for( i=0;i<10;i++)
+		{
+			System.out.println("i" +i);
+			
+			
+		}
+	}
+
+}
