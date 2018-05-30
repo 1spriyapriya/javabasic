d1ce3d8ad5b3a5dc14b77f73b7670051aded70e2
1spriyapriya <priyadharshinisekar001@gmail.com>
2018-05-30 11:08:56 +0530

    basic

---
+++ javabasics/src/com/basic/SwitchProgramExample.java
@@ -0,0 +1,21 @@
+package com.basic;
+
+public class SwitchProgramExample {
+	public static void main(String[]args)
+	{
+		int choice=20;
+		switch(choice)
+		{
+		case 10:
+			System.out.println("10");
+		break;
+		case 20:
+			System.out.println("20");
+			break;
+			default:
+				System.out.println("default");
+				break;
+		}
+	}
+
+}
