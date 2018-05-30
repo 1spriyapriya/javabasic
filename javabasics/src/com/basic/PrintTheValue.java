470f14f4578c5529be644f1504f1eb0ee0487507
1spriyapriya <priyadharshinisekar001@gmail.com>
2018-05-30 15:56:24 +0530

    basic

---
+++ javabasics/src/com/basic/PrintTheValue.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class PrintTheValue {
+	public static void main(String []args)
+	{
+		int n=1;
+		while(n<10)
+		{
+			n++;
+			System.out.println("n="+n);
+			
+			
+		}
+	}
+
+}
