c4cba4062fd34c1ae320e1c21c9927d7d732edba
1spriyapriya <priyadharshinisekar001@gmail.com>
2018-05-30 11:15:41 +0530

    basic

---
+++ javabasics/src/com/basic/WhileProgramExample.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class WhileProgramExample {
+	public static void main( String []args)
+	{
+		int y=0;
+		while(y<0)
+		
+			System.out.println("y=" +y);
+			y++;
+			System.out.println(y);
+		
+	}
+
+}
