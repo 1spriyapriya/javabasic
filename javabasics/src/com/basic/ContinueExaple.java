ce2aa4d239d2f33e37c2f2a531fc30af0aeabdeb
1spriyapriya <priyadharshinisekar001@gmail.com>
2018-05-30 15:26:01 +0530

    basic

---
+++ javabasics/src/com/basic/ContinueExaple.java
@@ -0,0 +1,29 @@
+package com.basic;
+
+public class ContinueExaple{
+	public static void main(String []args)
+	{
+		for(int i=0;i<11;i++)
+		{
+			System.out.println("+i");
+		}
+		for(int i=0;i<11;i++) {
+			
+			if(i==6) {
+				continue;
+			}
+		System.out.println(i);
+		if(i==8)
+		{
+			break;
+		}
+		}
+		
+		}
+	}
+	
+
+
+
+
+
