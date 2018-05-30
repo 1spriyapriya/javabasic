da726f57789fca4776fca5460db2197e79b67d30
1spriyapriya <priyadharshinisekar001@gmail.com>
2018-05-30 14:50:45 +0530

    basic

---
+++ javabasics/src/com/basic/LOopBreakExample.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class LOopBreakExample {
+	public static void main(String []args)
+	{
+		int i;
+		for(i=0;i<7;i++)
+		{
+			System.out.println(i);
+			if(i==5)
+			{
+				break;
+			}
+		}
+		
+	}
+
+}
