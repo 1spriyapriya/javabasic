fa90bd7abbef45d69f8da130558dc070ae753620
1spriyapriya <priyadharshinisekar001@gmail.com>
2018-06-01 16:00:29 +0530

    basic

---
+++ javabasics/src/com/basic/Array.java
@@ -0,0 +1,20 @@
+package com.basic;
+
+public class Array {
+public static void main(String[] args) {
+	int array[] = new int[10];
+    for (int count=0;count<10;count++){
+       array[count]=count+1;
+   
+   
+       System.out.println("array["+count+"] = "+array[count]);
+   }
+  
+ 
+  }
+
+		
+	
+	
+}
+
